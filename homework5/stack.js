b416f484e56f4ec7df342dbc3fbce3e75a762f78
Andrii Trubchanynov <univer1857@gmail.com>
2021-10-14 18:30:01 +0300

    add linked list and fix mistakes

--- homework5/stack.js
+++ homework5/stack.js
@@ -1,6 +1,6 @@
 class Stack {
   constructor(maxLength = 10) {
-    if (!Number.isInteger(maxLength)) {
+    if (!Number.isInteger(maxLength) || maxLength < 1) {
       throw new Error('invalid length');
     }
     this.length = 0;
@@ -10,7 +10,8 @@ class Stack {
 
   push(elem) {
     const prev = this.top;
-    const node = { prev, elem };
+    const node = new Node(elem);
+    node.prev = prev;
     this.top = node;
     if (this.length > this.maxLength) {
       throw new Error('stack is oversized');
@@ -60,6 +61,85 @@ class Stack {
     }
 
     return stack;
+  }
+}
+
+class Node {
+  constructor(elem) {
+    this.elem = elem;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  append(elem) {
+    if (this.length === 0) {
+      this.head = new Node(elem);
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = new Node(elem);
+    }
+
+    this.length++;
+  }
+
+  prepend(elem) {
+    const node = new Node(elem);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+  }
+
+  find(elem) {
+    let current = this.head;
+
+    while (current) {
+      if (current.elem === elem) {
+        return current.elem;
+      }
+
+      current = current.next;
+    }
+
+    return null;
+  }
+
+  toArray() {
+    let current = this.head;
+    const newArr = [];
+    
+    while (current) {
+      newArr.push(current.elem);
+      current = current.next;
+    }
+
+    return newArr;
+  }
+
+  static fromIterable(iterable) {
+    if (!iterable || typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error('param is not iterable');
+    }
+
+    const list = new LinkedList();
+
+    for (const value of iterable) {
+      list.append(value);  
+    }
+
+    return list;
+
   }
 }
 
